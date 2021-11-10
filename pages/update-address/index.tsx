ad154521decd119b341e9109e9eadfcc4a060d81
shellymutu-grigg <shellymutugrigg@gmail.com>
2021-11-10 20:52:40 +1300

    Issue#40: Correct styling

--- pages/update-address/index.tsx
+++ pages/update-address/index.tsx
@@ -3,11 +3,13 @@ import { Header } from '@components/Header/Header';
 import { Container } from '@material-ui/core';
 import { UpdateAddressPanel } from '@components/UpdateAddressPanel/UpdateAddressPanel';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useUpdateAddressPanelStyles } from '@components/UpdateAddressPanel/UpdateAddressPanel.styles';
 
 const UpdateAddressPage: FC = () => {
+  const { headerShadow } = useUpdateAddressPanelStyles();
   return (
     <>
-      <Header />
+      <Header hideProfile className={headerShadow} />
       <Container disableGutters>
         <UpdateAddressPanel buttonText="Back" />
       </Container>
--- src/components/UpdateAddressPanel/UpdateAddressPanel.tsx
+++ src/components/UpdateAddressPanel/UpdateAddressPanel.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from 'react';
 import Button from 'react-bootstrap/Button';
+import { useUpdateAddressPanelStyles } from './UpdateAddressPanel.styles';
 
 interface ButtonProps {
   buttonText: string;
 }
 
 export const UpdateAddressPanel: FC<ButtonProps> = ({ buttonText }) => {
+  const { padding } = useUpdateAddressPanelStyles();
   return (
-    <Button type="submit" variant="outline-info" href="/">
-      {' '}
-      {buttonText}{' '}
-    </Button>
+    <div className={padding}>
+      <Button type="submit" variant="outline-info" href="/">
+        {' '}
+        {buttonText}{' '}
+      </Button>
+    </div>
   );
 };
