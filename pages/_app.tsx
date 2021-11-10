355a59653117f97c5bb0afc63181d940c64c7fcf
shellymutu-grigg <shellymutugrigg@gmail.com>
2021-11-10 20:21:42 +1300

    Issue#40: Localise bootstrap to new component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import AppContextProvider from '@utils/app-context';
 import theme from '@theme/theme';
 import { ReactQueryClientProvider } from '@components/ReactQueryClientProvider/ReactQueryClientProvider';
 import '../styles/globals.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App(props: AppProps): JSX.Element {
   const { Component, pageProps } = props;
--- pages/update-address/index.tsx
+++ pages/update-address/index.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { Header } from '@components/Header/Header';
 import { Container } from '@material-ui/core';
 import { UpdateAddressPanel } from '@components/UpdateAddressPanel/UpdateAddressPanel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const UpdateAddressPage: FC = () => {
   return (
